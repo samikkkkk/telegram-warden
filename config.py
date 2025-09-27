5f221fb7e6fe49731fe6f412656a90fa7b14f28e
Samikague <osakwe0120@gmail.com>
2025-09-27 10:57:42 +0300

    config fix

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
 
+from aiogram import Bot
+
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     OWNER_ID = os.getenv("OWNER_ID")
     DB_URL = os.getenv("DATABASE_URL", "sqlite:///data/db.sqlite3")
+
+    bot = Bot(BOT_TOKEN)
     
     if not all([BOT_TOKEN, OWNER_ID]):
         raise ValueError("Config-value(s) not found.")
